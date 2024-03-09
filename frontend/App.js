a4c4d26785bf09aeb938ac6629cc1cd27dfed640
jaqlynai <119989117+jaqlynai@users.noreply.github.com>
2024-03-09 14:28:35 -0800

    Slider logs user's input as an int[0, 100]

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,34 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useSharedValue } from 'react-native-reanimated';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import Slider from '@react-native-community/slider';
 
 export default function App() {
-  // const[sliderMood, setSliderMood] = useState(0);
+  const[sliderMood, setSliderMood] = useState(0);
+
+  const handleSlideComplete = (endValue) => {
+    setSliderMood(Math.round(endValue));
+
+    console.log("slidermood: " + sliderMood);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
+      <Text>log your mood</Text>
       <StatusBar style="auto" />
       <Slider 
         style = {{width: 300, height: 40}} 
         minimumValue = {0} 
         maximumValue = {100}
-        minimumTrackTintColor = "#FF482B"
-        maximumTrackTintColor = "#2B5BFF"
+        minimumTrackTintColor = "#900C3F"
+        maximumTrackTintColor = "#FFC300"
+        onValueChange={handleSlideComplete}
         />
+      <Pressable
+        style = {styles.button}
+        onPress={() => {console.log("mood confirmed")}}>
+          <Text>confirm</Text>
+      </Pressable>
     </View>
   );
 }
@@ -24,12 +36,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EDF5AF',
     alignItems: 'center',
     justifyContent: 'center',
   },
   slider: {
     flex: 1,
     padding: 20
+  },
+  button: {
+    backgroundColor: "#FF5733",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 12
   }
 });
