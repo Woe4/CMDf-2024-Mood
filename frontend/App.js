d63c5507dda8102037ac10a11e3b661ede02dc4f
jaqlynai <119989117+jaqlynai@users.noreply.github.com>
2024-03-09 17:58:43 -0800

    Merged frontend-b with frontend

--- frontend/App.js
+++ frontend/App.js
@@ -4,16 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LogMoodScreen from './LogMood';
 import Notification from './Notification';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SentimentScreen from './screens/SentimentScreen';
 
-const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name = "Home" component = {HomeScreen}/>
         <Tab.Screen name = "Log Mood" component = {LogMoodScreen}/>
         <Tab.Screen name = "Notifications" component = {Notification}/>
       </Tab.Navigator>
