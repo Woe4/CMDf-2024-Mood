9153fe2de9ebfe1e76cf84b91fe67f410ca41d3d
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-03-10 03:51:03 -0700

    Add apollo client in app.js (no error but doesn't send req)

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,17 @@
-import {createContext, useState} from 'react';
+import React, {createContext, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LogMoodScreen from './LogMood';
 import generateNotification from './Notification';
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from './Home';
+import {AppRegistry} from 'react-native';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5002/graphql',
+    cache: new InMemoryCache()
+});
 
 const Tab = createBottomTabNavigator();
 export const UserContext = createContext(null);
@@ -14,18 +21,21 @@ export default function App() {
 
     const [user, setUser] = useState({name: "", email: ""});
     return (
-        <UserContext.Provider value={user}>
-            {user.email
-                ? <NavigationContainer>
-                    <Tab.Navigator>
-                        <Tab.Screen name="Home" component={HomeScreen}/>
-                        <Tab.Screen name="Log Mood" component={LogMoodScreen}/>
-                    </Tab.Navigator>
-                </NavigationContainer>
-                : <LoginScreen setUser={setUser}/>
-            }
-        </UserContext.Provider>
+        <ApolloProvider client={client}>
+            <UserContext.Provider value={user}>
+                {user.email
+                    ? <NavigationContainer>
+                        <Tab.Navigator>
+                            <Tab.Screen name="Home" component={HomeScreen}/>
+                            <Tab.Screen name="Log Mood" component={LogMoodScreen}/>
+                        </Tab.Navigator>
+                    </NavigationContainer>
+                    : <LoginScreen setUser={setUser}/>
+                }
+            </UserContext.Provider>
+        </ApolloProvider>
     );
 
 }
 
+AppRegistry.registerComponent('MyApplication', () => App);
