6fe83b3de08d4fb40a2cb1f99df3eb100d034842
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-03-09 23:25:01 -0800

    Login screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,28 @@
-import { useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createContext, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LogMoodScreen from './LogMood';
 import Notification from './Notification';
+import LoginScreen from "./screens/LoginScreen";
 
 const Tab = createBottomTabNavigator();
+const UserContext = createContext(null);
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name = "Log Mood" component = {LogMoodScreen}/>
-        <Tab.Screen name = "Notifications" component = {Notification}/>
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    const [user, setUser] = useState({name: "", email: ""});
+    return (
+        <UserContext.Provider value={user}>
+            {user.email
+                ? <NavigationContainer>
+                    <Tab.Navigator>
+                        <Tab.Screen name="Log Mood" component={LogMoodScreen}/>
+                        <Tab.Screen name="Notifications" component={Notification}/>
+                    </Tab.Navigator>
+                </NavigationContainer>
+                : <LoginScreen setUser={setUser}/>
+            }
+        </UserContext.Provider>
+    );
 
 }
 
