c2fb3c91f17537e3fb5554467ae2d8b6a00035a8
jaqlynai <119989117+jaqlynai@users.noreply.github.com>
2024-03-09 17:18:20 -0800

    Set up push notifications and permission requests from Android/iOS

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LogMoodScreen from './LogMood';
+import Notification from './Notification';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,10 +22,11 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name = "Home" component = {HomeScreen}/>
         <Tab.Screen name = "Log Mood" component = {LogMoodScreen}/>
+        <Tab.Screen name = "Notifications" component = {Notification}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
-  
+
 }
 
 const styles = StyleSheet.create({
