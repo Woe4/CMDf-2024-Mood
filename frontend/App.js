f624e158fcd8db49f7f74b5de83ed76e963f53c6
June <tyu12@students.cs.ubc.ca>
2024-03-10 03:38:15 -0700

    Setup done

--- frontend/App.js
+++ frontend/App.js
@@ -6,16 +6,29 @@ import generateNotification from './Notification';
 
 const Tab = createBottomTabNavigator();
 
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: "http://128.189.91.71:5002/graphql",
+  cache: new InMemoryCache()
+});
+
 export default function App() {
   generateNotification();
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name = "Home" component = {HomeScreen}/>
-        <Tab.Screen name = "Log Mood" component = {LogMoodScreen}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name = "Home" component = {HomeScreen}/>
+          <Tab.Screen name = "Log Mood" component = {LogMoodScreen}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
+AppRegistry.registerComponent('MoodTracker', () => App);
--- frontend/Home.js
+++ frontend/Home.js
@@ -5,14 +5,19 @@ import Slider from '@react-native-community/slider';
 import ImageButton from './components/ImageButton';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
+import { gql, useMutation, useQuery } from '@apollo/client';
 
-
-
+const GET_USER = gql`
+  query getUserData($email: String!) {
+    userByEmail(email: $email) {
+      name
+    }
+  }
+  `;
 
 const Stack = createNativeStackNavigator();
 
 export default function HomeScreenStack() {
-
     return (
         <Stack.Navigator
             screenOptions = {{headerShown: false}}
@@ -35,95 +40,101 @@ export default function HomeScreenStack() {
 
 
 function HomeScreen({ navigation }) {
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: { "email": "jen@jen.com" }
+  });
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error! ${error.message} oof</Text>;
 
-    return (
-        <View
-            style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'space-around',
-          }}>
-            <Text>welcome!</Text>
-            <Pressable
-            style = {styles.button}
-            onPress={() => navigation.navigate("stats")}>
-            <Text>stats</Text>
-        </Pressable>
-        </View>
-    )
+  return (
+      <View
+          style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+          <Text>Welcome {data.userByEmail.name}!</Text>
+          <Text></Text>
+          <Pressable
+          style = {styles.button}
+          onPress={() => navigation.navigate("stats")}>
+          <Text>stats</Text>
+      </Pressable>
+      </View>
+  )
 }
 
 
 function StatsScreen({ navigation }) {
 
-    return (
-        <View
-            style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'space-around',
-          }}>
-            <Text>stats</Text>
-            <Pressable
-            style = {styles.button}
-            onPress={() => navigation.navigate("history")}>
-            <Text>history</Text>
-            </Pressable>
-            <Pressable
-            style = {styles.button}
-            onPress={() => navigation.goBack()}>
-            <Text>back</Text>
-        </Pressable>
-        </View>
-    )
+  return (
+      <View
+          style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+          <Text>stats</Text>
+          <Pressable
+          style = {styles.button}
+          onPress={() => navigation.navigate("history")}>
+          <Text>history</Text>
+          </Pressable>
+          <Pressable
+          style = {styles.button}
+          onPress={() => navigation.goBack()}>
+          <Text>back</Text>
+      </Pressable>
+      </View>
+  )
 }
 
 
 function HistoryScreen({ navigation }) {
   const [selected, setSelected] = useState('');
 
-    return (
-        <View
-            style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'space-around',
-          }}>
-            <Text>history</Text>
-
-        {/* <FlatList
-        data={[
-          {key: 'mango'}
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-      /> */}
-        <Calendar
+  return (
+      <View
           style={{
-            borderWidth: 1,
-            borderColor: 'black',
-            height: 360,
-            width: 350
-          }}
-          onDayPress={day => {
-          setSelected(day.dateString);
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+          <Text>history</Text>
+
+      {/* <FlatList
+      data={[
+        {key: 'mango'}
+      ]}
+      renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+    /> */}
+      <Calendar
+        style={{
+          borderWidth: 1,
+          borderColor: 'black',
+          height: 360,
+          width: 350
         }}
-        markedDates={{
-          '2024-03-10': {selected: true, marked: false, selectedColor: '#FF6961'},
-          '2024-03-11': {selected: true, marked: false, selectedColor: '#FAC898'},
-          '2024-03-12': {selected: true, marked: false, selectedColor: '#FDFD96'},
-          '2024-03-13': {selected: true, marked: false, selectedColor: '#77DD77'},
-          '2024-03-14': {selected: true, marked: false, selectedColor: '#A5D7F5'},
-          '2024-03-15': {selected: true, marked: false, selectedColor: '#8974D0'},
-          '2024-03-16': {selected: true, marked: false, selectedColor: '#C3B1E1'}
-        }}
-        />
-        <Pressable
-            style = {styles.button}
-            onPress={() => navigation.goBack()}>
-            <Text>back</Text>
-        </Pressable>
-        </View>
-    )
+        onDayPress={day => {
+        setSelected(day.dateString);
+      }}
+      markedDates={{
+        '2024-03-10': {selected: true, marked: false, selectedColor: '#FF6961'},
+        '2024-03-11': {selected: true, marked: false, selectedColor: '#FAC898'},
+        '2024-03-12': {selected: true, marked: false, selectedColor: '#FDFD96'},
+        '2024-03-13': {selected: true, marked: false, selectedColor: '#77DD77'},
+        '2024-03-14': {selected: true, marked: false, selectedColor: '#A5D7F5'},
+        '2024-03-15': {selected: true, marked: false, selectedColor: '#8974D0'},
+        '2024-03-16': {selected: true, marked: false, selectedColor: '#C3B1E1'}
+      }}
+      />
+      <Pressable
+          style = {styles.button}
+          onPress={() => navigation.goBack()}>
+          <Text>back</Text>
+      </Pressable>
+      </View>
+  )
 }
 
 
