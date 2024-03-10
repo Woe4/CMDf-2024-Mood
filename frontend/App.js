5410f53f64835d281d2a63f9fd1d562a14a5372a
ariana-hafezi <159683905+ariana-hafezi@users.noreply.github.com>
2024-03-09 16:41:32 -0800

    frontend work

--- frontend/App.js
+++ frontend/App.js
@@ -1,37 +1,25 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LogMoodScreen from './LogMood';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LogMoodScreen from './screens/LogMoodScreen';
+import SentimentScreen from './screens/SentimentScreen';
 
-const Tab = createBottomTabNavigator();
-
-function HomeScreen() {
-  return (
-    <View style = {styles.container}>
-      <Text>Welcome home!!</Text>
-    </View>
-  )
-}
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name = "Home" component = {HomeScreen}/>
-        <Tab.Screen name = "Log Mood" component = {LogMoodScreen}/>
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen
+        name="log mood"
+        component={LogMoodScreen}
+        />
+        <Stack.Screen
+        name="sentiment"
+        component={SentimentScreen}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EDF5AF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
